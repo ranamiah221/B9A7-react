d9a25a891978736f014aa11bd87dab1599a4c021
ranamiah221 <ranarasul21@gmail.com>
2024-09-27 17:24:02 +0600

    added to the cart in dynamic list ingredients

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,7 @@
 
 const Cart = ({cart}) => {
     const {recipe_image, recipe_name, short_description, ingredients,preparing_time,calories}=cart;
+    
     return (
         <div className="card bg-base-100 w-full shadow-xl p-7 rounded-lg">
         <figure>
@@ -11,7 +12,13 @@ const Cart = ({cart}) => {
         <div className="card-body">
           <h2 className="text-xl font-medium mt-4 mb-2">{recipe_name}</h2>
           <p className="text-[#878787] text-base font-normal border-b-2 pb-5">{short_description}</p>
-         
+         <ul className="text-base text-[#878787] font-normal list-disc pl-7 border-b-2 py-5">
+            <li>{ingredients[0]}</li>
+            <li>{ingredients[1]}</li>
+            <li>{ingredients[2]}</li>
+            <li>{ingredients[3]}</li>
+
+         </ul>
         </div>
       </div>
     );
