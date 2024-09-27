6757ee81db043511ee09453982d48fb303ae6137
ranamiah221 <ranarasul21@gmail.com>
2024-09-27 15:56:34 +0600

    added rercipes section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Header from './components/Header/Header'
+import Recipes from './components/Recipes/Recipes'
 
 function App() {
 
@@ -9,6 +10,8 @@ function App() {
     <>
        <Header></Header>
        <Banner></Banner>
+       <Recipes></Recipes>
+
     </>
   )
 }
---
+++ src/components/Recipes/Recipes.jsx
@@ -0,0 +1,11 @@
+
+const Recipes = () => {
+    return (
+        <div className="text-center w-2/4 mx-auto mt-16 mb-10">
+            <h2 className="text-4xl font-bold mb-5">Our Recipes</h2>
+            <p>Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque.</p>
+        </div>
+    );
+};
+
+export default Recipes;
